e5780e4713836e6ba2a3e8aad957cfe410090e6d
A1UM1 <th.lakchai@gmail.com>
2026-08-25 13:38:44 +0700

    add spacing

--- client/src/lib/relativeTime.ts
+++ client/src/lib/relativeTime.ts
@@ -1,5 +1,6 @@
 import { formatRelative } from "date-fns";
 import { th } from "date-fns/locale";
+
 export default function formatRelativeTime(date: Date | string | number): string {
   return formatRelative(date, new Date(), { locale: th });
 }
