1d6efa81363ce90e18506b5aed5e5102e9c62dee
A1UM1 <88581492+a1um1@users.noreply.github.com>
2026-09-02 17:23:20 +0700

    create alert component

---
+++ client/src/components/alert/alert.stories.tsx
@@ -0,0 +1,11 @@
+import Alert from "#/components/alert/alert";
+
+const meta = {
+  title: "Components/Alert",
+  component: Alert,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Playground = {};
