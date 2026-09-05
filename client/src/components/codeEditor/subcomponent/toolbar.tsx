53b763953f152f1d46b949b20538bd83689846b1
A1UM1 <88581492+a1um1@users.noreply.github.com>
2026-09-05 11:48:30 +0700

    fix: correct spelling of "Processing" in EditorToolbar component; improve Dockerfile comments for clarity

--- client/src/components/codeEditor/subcomponent/toolbar.tsx
+++ client/src/components/codeEditor/subcomponent/toolbar.tsx
@@ -64,7 +64,7 @@ export default function EditorToolbar() {
             {isMutatingOrLoading ? (
               <>
                 <Spinner />
-                Procsessing
+                Proccessing
               </>
             ) : (
               <>
