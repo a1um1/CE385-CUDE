0a3d765d728aa60837ea4b96947784e766d76200
Barkro48 <siwaton548@gmail.com>
2026-09-06 16:14:07 +0700

    แก้

--- client/src/components/selector/selector.tsx
+++ client/src/components/selector/selector.tsx
@@ -1,33 +1,21 @@
 import styles from "./selector.module.css";
-import React from 'react';
+import React from "react";
 import clsx from "clsx";
 
-
-
-
-export interface SelectorProps 
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>
- {
+export interface SelectorProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   iconUrl?: string;
   label: string;
   isActive?: boolean;
-
 }
 
-export function Selector({iconUrl, label, isActive = false, ...props}: SelectorProps) {
+export function Selector({ iconUrl, label, isActive = false, ...props }: SelectorProps) {
+  const className = clsx(styles.card, isActive && styles.selected ,props.className);
 
-  const className = clsx(styles.card, isActive && styles.selected);
-    
-return (
-    <button
-      type="button"
-      className={className}
-      aria-pressed={isActive}
-      {...props}
-    >
+  return (
+    <button type="button" aria-pressed={isActive} {...props} className={className}>
       {iconUrl && (
         <div className={styles.iconWrapper}>
-          <img src={iconUrl} alt="icon"/>
+          <img src={iconUrl} alt="icon" />
         </div>
       )}
       <span className={styles.label}>{label}</span>
