58ba1ff4f029531641041cabc9e1163ab52acdb0
A1UM1 <88581492+a1um1@users.noreply.github.com>
2026-09-17 15:37:33 +0700

    feat: move ExtractRequestBody and ExtractRequestQuery types to a new file for better organization

--- client/src/data/base/baseAPI.ts
+++ client/src/data/base/baseAPI.ts
@@ -59,27 +59,4 @@ export const APIclient = createClient<paths>({
   credentials: "include",
 });
 
-export type ExtractRequestBody<
-  Path extends keyof paths,
-  Method extends keyof paths[Path],
-  ContentType extends string = "application/json",
-> = paths[Path][Method] extends {
-  requestBody?: {
-    content: Record<ContentType, infer BodyType>;
-  };
-}
-  ? BodyType
-  : never;
-
-export type ExtractRequestQuery<
-  Path extends keyof paths,
-  Method extends keyof paths[Path],
-> = paths[Path][Method] extends {
-  parameters?: {
-    query?: infer QueryType;
-  };
-}
-  ? QueryType
-  : never;
-
 APIclient.use(authMiddleware);
