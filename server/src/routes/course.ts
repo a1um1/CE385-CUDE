f6fd063d7c3bb2138e4eca0c049bc0dd711862d3
Barkro48 <siwaton548@gmail.com>
2026-09-18 12:25:43 +0700

    แก้ตาม ที่สั่ง

--- server/src/routes/course.ts
+++ server/src/routes/course.ts
@@ -1,8 +1,6 @@
 import CoursesController from "#/controller/admin/courses";
-import { AdminCourseQuerySchema } from "#/controller/admin/courses/courses.schema";
 import { z } from "#/lib/extendZod";
 import CustomRouter from "#/lib/router/customRouter";
-import UserError from "#/lib/router/http/userError";
 
 const publicCourseSchema = z.object({
   id: z.string().openapi({ example: "course_id" }),
@@ -21,35 +19,18 @@ const courseRouter = new CustomRouter({
   prefix: "/course",
   tags: ["Course"],
   authentication: true,
-})
-  .get(
-    "/",
-    {
-      summary: "List all available courses",
-      query: AdminCourseQuerySchema,
-      response: publicCourseResponseSchema,
-    },
-    async ({ query, status }) => {
-      try {
-        const result = await CoursesController.getPaginateLists(query);
-        status.set(200); // กำหนด status OK
-        return {
-          ...result,
-        };
-      } catch (error) {
-        console.error("Error fetching courses:", error);
-        throw new UserError(500, "Failed to fetch courses");
-      }
-    },
-  )
-  .get(
-    "/usererror",
-    {
-      summary: "Handled Error Example",
-    },
-    async () => {
-      throw new UserError(404, "course not found");
-    },
-  );
+}).get(
+  "/",
+  {
+    summary: "List all available courses",
+    response: publicCourseResponseSchema,
+  },
+  async () => {
+    const result = await CoursesController.getAll();
+    return {
+      ...result,
+    };
+  },
+);
 
 export const courseRoute = courseRouter.route;
